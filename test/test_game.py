fb69f1d7fb5e1b2fea99994bc2072e0858544f8a
Xritsos <kobecris24@yahoo.gr>
2024-06-02 14:51:58 +0300

    updated to match new source of flappy_bird

--- test/test_game.py
+++ test/test_game.py
@@ -7,52 +7,46 @@ from pygame_screen_record import ScreenRecorder
 from pygame_screen_record.ScreenRecorder import cleanup
 
 sys.path.append('./')
-from source.game import wrapped_flappy_bird as game
-from source.utils import Image 
-
+from source.game.flappy_bird import FlappyBird
+from source.utils.process_image import pre_processing
 
 def test(test_id):
     torch.manual_seed(22)
+    torch.cuda.manual_seed(22)
     device = "cuda"
+    IMAGE_SIZE = 84
     
     with torch.no_grad():
         model = torch.load(f'./model_ckpts/{test_id}_model.pt')
         
-        game_state = game.GameState()
-        number_of_actions = 2
+        game_state = FlappyBird()
+        image, reward, terminal = game_state.next_frame(0, disp_score=True)
+        image = pre_processing(image[:game_state.screen_width, :int(game_state.base_y)], 
+                               IMAGE_SIZE, IMAGE_SIZE)
         
-        action = torch.zeros([number_of_actions], dtype=torch.float32).to(device)
-        action[0] = 1
+        image = torch.from_numpy(image).to(device)
         
-        image_data, reward, terminal = game_state.frame_step(action)
-        image_data = Image.resize_and_bgr2gray(image_data)
-        image_data = Image.image_to_tensor(image_data)
-        image_data = image_data.to(device)
-        state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
+        state = torch.cat(tuple(image for _ in range(4)))[None, :, :, :]
 
         recorder = ScreenRecorder(30) # pass your desired fps
         recorder.start_rec() # start recording
         try:
-            while True:
+            while not terminal:
                 # Get output from the neural network
-                output = model(state)[0]
-
-                action = torch.zeros([number_of_actions], dtype=torch.float32).to(device)
-
-                # Get action
-                action_index = torch.argmax(output)
-                action_index = action_index.to(device)
+                with torch.no_grad():
+                    prediction = model(state)[0]
                 
-                action[action_index] = 1
+                action = torch.argmax(prediction).item()
 
-                # Get next state
-                image_data_1, reward, terminal = game_state.frame_step(action)
-                image_data_1 = Image.resize_and_bgr2gray(image_data_1)
-                image_data_1 = Image.image_to_tensor(image_data_1)
-                image_data_1 = image_data_1.to(device)
-                state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+                next_image, reward, terminal = game_state.next_frame(action, disp_score=True)
+                next_image = pre_processing(next_image[:game_state.screen_width, :int(game_state.base_y)], 
+                                    IMAGE_SIZE, IMAGE_SIZE)
+        
+                next_image = torch.from_numpy(next_image).to(device)
+        
+                next_state = torch.cat((state[0, 1:, :, :], next_image))[None, :, :, :]
 
-                state = state_1
+                state = next_state
         finally:
             recorder.stop_rec()	# stop recording
             recorder.save_recording(f"./videos/{test_id}.avi") # saves the last recording
