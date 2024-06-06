8b96ff34f93357a05a48122b6aef55c2b0e0b890
Xritsos <kobecris@yahoo.gr>
2024-06-07 01:46:01 +0300

    updated

--- source/DoubleDQN.py
+++ source/DoubleDQN.py
@@ -249,7 +249,7 @@ def train(test_id):
 if __name__ == "__main__":
     
     # test_id = int(input("Set test_id> "))
-    for test_id in [19, 20, 21]:
+    for test_id in [21]:
         plt.ion()
         
         train(test_id)
